f3a1572c8af23842571f1d63ba4d68dd6c8944fd
Lycoder <aurealinbox@gmail.com>
2021-02-07 17:14:50 -0300

    Initial commit

---
+++ color.hpp
@@ -0,0 +1,83 @@
+/**
+ *  \file color.hpp
+ *  \brief Defines pixel format conversion primitives, implements a list of colors
+ *         depending on the pixel format chosen, and color encoding functions
+ */
+
+#pragma once
+
+#include <cstdint>
+
+namespace lgw {
+    namespace detail {
+#ifdef LGW_FORMAT_ARGB8888
+        static const size_t
+            conv_alpha_shift = 24,
+            conv_red_shift   = 16,
+            conv_green_shift = 8,
+            conv_blue_shift  = 0;
+#endif
+
+#ifdef LGW_FORMAT_RGBA8888
+        static const size_t
+            conv_red_shift   = 24,
+            conv_green_shift = 16,
+            conv_blue_shift  = 8
+            conv_alpha_shift = 0;
+#endif
+    }
+	
+    namespace color {
+#ifdef LGW_FORMAT_ARGB8888
+		static const constexpr uint32_t
+			transparent = 0x00000000,
+			black 		= 0xff000000,
+			white 		= 0xffffffff,
+			red 		= 0xffff0000,
+			green 		= 0xff00ff00,
+			blue 		= 0xff0000ff,
+			yellow 		= 0xffffff00,
+			magenta 	= 0xffff00ff,
+			cyan 		= 0xff00ffff;
+#endif
+
+#ifdef LGW_FORMAT_RGBA8888
+		static const constexpr uint32_t
+			transparent = 0x00000000,
+			black 		= 0x000000ff,
+			white 		= 0xffffffff,
+			red 		= 0xff0000ff,
+			green 		= 0x00ff00ff,
+			blue 		= 0x0000ffff,
+			yellow 		= 0xffff00ff,
+			magenta 	= 0xff00ffff,
+			cyan 		= 0x00ffffff;
+#endif
+	}
+
+	/**
+	 *  \brief Generate a color given RGB components
+	 *  
+	 *  \param r Color's red component
+	 *  \param g Color's green component
+	 *  \param b Color's blue component
+	 */
+	inline uint32_t color(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 0xff) {
+		return (a << detail::conv_alpha_shift) |
+               (r << detail::conv_red_shift)   |
+               (g << detail::conv_green_shift) |
+               (b << detail::conv_blue_shift);
+	}
+	
+	/**
+	 *  \brief Generate a color given a Y value (r = g = b = y)
+	 *  
+	 *  \param y Color's grayscale value
+	 */
+	inline uint32_t color(uint8_t y, uint8_t a = 0xff) {
+        return (a << detail::conv_alpha_shift) |
+               (y << detail::conv_red_shift)   |
+               (y << detail::conv_green_shift) |
+               (y << detail::conv_blue_shift);
+	}
+}
---
+++ framebuffer.hpp
@@ -0,0 +1,88 @@
+/**
+ *  \file framebuffer.hpp
+ *  \brief Defines a generic framebuffer, and implements a lightweight 32-bit color specialization
+ */
+
+#pragma once
+
+#include <array>
+#include <cstdint>
+
+#include "color.hpp"
+
+namespace lgw {
+	namespace detail {
+		/**
+		 *  \class detail::framebuffer
+		 *  \brief A format-agnostic/generic framebuffer, meant to be inherited and implemented
+		 */
+		template <typename T, size_t W, size_t H> class framebuffer {
+			typedef std::array <T, W * H> buf_t;
+
+		public:
+			virtual void clear(T) = 0;
+			virtual void init(T) = 0;
+			virtual void draw(size_t, size_t, T) = 0;
+			virtual T& read(size_t, size_t) = 0;
+			virtual T* get_buffer() = 0;
+		};
+	}
+
+	/**
+	 *  \brief A 32-bit color pixel framebuffer
+	 */
+	template <size_t W, size_t H> class framebuffer : public detail::framebuffer<uint32_t, W, H> {
+		uint32_t dummy = 0;
+
+		buf_t buf;
+
+	public:
+		framebuffer() = default;
+		framebuffer(uint32_t color) { init(c); }
+
+		/**
+		 *  \brief Clear the framebuffer, and fill with the specified color
+		 *  
+		 *  \param color Color to set all pixels in the framebuffer to
+		 */
+		void clear(uint32_t color = color::black) override {
+			for (uint32_t& p : buf) p = color;
+		}
+
+		/**
+		 *  \brief Draw a pixel at location (x, y) with the specified color
+		 *  
+		 *  \param x Pixel X coord
+		 *  \param y Pixel Y coord
+		 *  \param color Color to set the pixel to
+		 */
+		void draw(size_t x, size_t y, uint32_t color) override {
+			size_t index = x + (y * W);
+
+			if (index > m_buf.size()) return;
+
+			buf[index] = color;
+		}
+
+		/**
+		 *  \brief Get a reference to the pixel at location (x, y)
+		 *  
+		 *  \param x Pixel X coord
+		 *  \param y Pixel Y coord
+		 */
+		uint32_t& read(size_t x, size_t y) override {
+			size_t index = x + (y * W);
+
+			if (index > m_buf.size()) return dummy;
+
+			return buf.at(index);
+		}
+
+		/**
+		 *  \brief Get a pointer to the underlying array
+		 */
+		uint32_t* get_buffer() override {
+			return buf.data();
+		}
+	};
+}
